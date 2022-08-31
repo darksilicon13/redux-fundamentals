d56aa39f6223bc77332c3a4d56f8d393f0e59484
darksilicon13 <darksilicon@outlook.com>
2022-08-31 10:40:21 +0900

    changed store to configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunkMiddleware from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from './reducer';
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+// const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
-const store = createStore(rootReducer, composedEnhancer);
+// const store = createStore(rootReducer, composedEnhancer);
+
+const store = configureStore({reducer: rootReducer})
 
 export default store;
